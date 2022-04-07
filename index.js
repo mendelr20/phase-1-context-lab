15790f316bd921229f4de042d9edd4131d25dc0a
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2022-04-06 23:42:37 -0400

    done

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }    
+}
 
+function createEmployeeRecords(records){
+    return records.map(rec => createEmployeeRecord(rec))
+}
+
+function createTimeInEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const inEvent = {
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeInEvents.push(inEvent)
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    const [date, hour] = dateStamp.split(" ")
+    const outEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    }
+    this.timeOutEvents.push(outEvent)
+    return this
+}
+
+function hoursWorkedOnDate(targetDate){
+    const inEvent = this.timeInEvents.find(inEvent => inEvent.date === targetDate)
+    const outEvent = this.timeOutEvents.find(oEvent => oEvent.date === targetDate)
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+function wagesEarnedOnDate(dateStamp){
+    return hoursWorkedOnDate.call(this, dateStamp) * this.payPerHour
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -17,7 +62,16 @@ const allWagesFor = function () {
     const payable = eligibleDates.reduce(function (memo, d) {
         return memo + wagesEarnedOnDate.call(this, d)
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
     return payable
+
 }
 
+function findEmployeeByFirstName(collection, firstNameString){
+    return collection.find(collection => collection.firstName === firstNameString)
+}
+
+function calculatePayroll(records){
+    return records.reduce((total, rec) => {
+        return total + allWagesFor.call(rec)
+    }, 0)
+}
